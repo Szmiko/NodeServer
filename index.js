295a89f79b0cf4392a4946e426b6cd63967ed810
Szymon Grabowski <szmiko@wp.pl>
2018-05-12 20:14:36 +0200

    Task

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ server.on('request', function(request, response) {
 	response.setHeader("Content-Type", "text/html; charset=utf-8");
 	if (request.method === 'GET' && request.url === '/hello') {
 		fs.readFile('./index.html', 'utf-8', function(err, data) {
-		response.write(data);
-		response.end();
-	});
-	else {
+			response.write(data);
+			response.end();
+		}) 
+	} else {
 		response.statusCode = 404;
 		response.write('<h1>404: Zła ścieżka!</h1>');
 		response.end();
 	};
 };
-server.listen(8080);
+server.listen(9000);
